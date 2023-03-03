71251e0beaed75ecead7d16b75ec05ee33a2faef
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2023-03-03 06:04:31 +0300

    Delete __init__.py

--- avsub/core/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-#
-# This file is part of AVsub
-# See https://github.com/serhatcelik/avsub for more information
-# Released under the GNU General Public License v3.0
-# Copyright (C) Serhat Çelik
-
-"""Core modules."""
