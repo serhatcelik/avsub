98180ffb160a560c203523988b2f6b3c39f5eadc
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2023-03-22 23:22:32 +0300

    Add files via upload

---
+++ avsub/cli/actions.py
@@ -0,0 +1,29 @@
+"""Custom actions."""
+
+import argparse
+from urllib.error import URLError
+from urllib.request import urlopen
+
+
+class CheckForUpdates(argparse.Action):
+    """Check for program updates."""
+
+    def __init__(self, current: str, url: str, **kwargs):
+        super().__init__(nargs=0, **kwargs)
+
+        self.current = current
+        self.url = url
+
+    def __call__(self, parser, *args):
+        try:
+            with urlopen(self.url, timeout=9) as answer:
+                latest = answer.readline().rstrip().decode()
+        except URLError as err:
+            print('[!]', err)
+        else:
+            if self.current == latest:
+                print('[*]', 'Up to date!')
+            else:
+                print('[*]', f'Update available: {self.current} -> {latest}')
+        finally:
+            parser.exit()
