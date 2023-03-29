5e112d76e9476da6aedce9b70ba725883d0d6321
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2023-03-29 05:03:40 +0300

    Update actions.py

--- avsub/actions.py
+++ avsub/actions.py
@@ -1,16 +1,16 @@
 """Custom actions."""
 
 import argparse
-from typing import Any, Callable
+from typing import Any, Callable, Iterable
 
 
 class DoAndExitAction(argparse.Action):
 
-    def __init__(self, func: Callable, fargs: tuple[Any, ...] = (), **kwargs):
+    def __init__(self, func: Callable, args: Iterable[Any] = (), **kwargs):
         super().__init__(**kwargs)
 
         self.func = func
-        self.fargs = fargs
+        self.args = args
 
     def __call__(self, parser, *args):
-        parser.exit(self.func(*self.fargs))
+        parser.exit(self.func(*self.args))
