09302f5d0a2034dc2c79c5ae954e34ce8df3449f
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2023-04-13 03:45:13 +0000

    Update cli.py

--- avsub/cli.py
+++ avsub/cli.py
@@ -14,7 +14,7 @@ from avsub.utils import check_for_updates
 
 parser = argparse.ArgumentParser(
     prog='avsub',
-    usage='python -m %(prog)s <extension> [<option> ...]',
+    usage='%(prog)s <extension> [<option> ...]',
     description='%(prog)s — a simplified command-line interface for ffmpeg',
     epilog='https://github.com/serhatcelik/avsub',
     formatter_class=argparse.RawTextHelpFormatter,
